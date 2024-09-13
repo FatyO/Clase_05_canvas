0e9549ec2f2712405a8d4c78c208c824019dcb74
Fatima Oviedo <foviedo@centro.edu.mx>
2024-09-12 18:59:00 -0600

    reto0

---
+++ reto/main.js
@@ -0,0 +1,11 @@
+const PI2 = Math.PI * 2;
+const CANVAS = document.getElementById("lienzo");
+const CTX = CANVAS.getContext("2d");
+CANVAS.width = window.innerWidth;
+CANVAS.height = window.innerHeight;
+
+function updateCanvasSize() {
+  CANVAS.width = CANVAS.getBoundingClientRect().width;
+  CANVAS.height = CANVAS.getBoundingClientRect().height;
+}
+
